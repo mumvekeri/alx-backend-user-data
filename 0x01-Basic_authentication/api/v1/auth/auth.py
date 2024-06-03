2a553670ee75200f73f6dac62b44de8d7f311019
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-06-04 00:17:23 +0200

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,43 +1,58 @@
-#!/usr/bin/env 
-""" Auth class
+#!/usr/bin/env python3
+"""
+Definition of class Auth
 """
 from flask import request
+from typing import (
+    List,
+    TypeVar
+)
 
 
 class Auth:
-    def require_auth(self, path: str, excluded_paths: list) -> bool:
+    """
+    Manages the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Check if authentication is required for a given path.
-
+        Determines whether a given path requires authentication or not
         Args:
-            path (str): The path to check.
-            excluded_paths (list): List of paths that are excluded from authentication.
-
-        Returns:
-            bool: True if authentication is required, False otherwise.
+            - path(str): Url path to be checked
+            - excluded_paths(List of str): List of paths that do not require
+              authentication
+        Return:
+            - True if path is not in excluded_paths, else False
         """
-        return False
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
-        Get the authorization header from the request.
-
-        Args:
-            request (flask.Request): The Flask request object.
-
-        Returns:
-            str: The authorization header.
+        Returns the authorization header from a request object
         """
-        return None
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
 
-    def current_user(self, request=None):
+    def current_user(self, request=None) -> TypeVar('User'):
         """
-        Get the current user from the request.
-
-        Args:
-            request (flask.Request): The Flask request object.
-
-        Returns:
-            TypeVar('User'): The current user.
+        Returns a User instance from information from a request object
         """
         return None

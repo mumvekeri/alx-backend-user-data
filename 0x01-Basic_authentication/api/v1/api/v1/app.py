10a8b3b45a26e8ac0ffa2ad27c8918c76ec0fd5b
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-06-04 00:20:57 +0200

    Update app.py

--- 0x01-Basic_authentication/api/v1/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
+from api.v1.auth.auth import Auth
 from flask_cors import (CORS, cross_origin)
 import os
 
@@ -19,6 +20,22 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.before_request
+def before_request_handler():
+    """Before request handler"""
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth is None:
+        return
+    if request.path in excluded_paths or not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":

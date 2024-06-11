1f9aae447db5f98ac8ffcfcba54dfe1f2123b6d8
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-06-11 21:16:45 +0200

    Create db.py

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,43 @@
+from user import User
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database
+        
+        Args:
+            email (str): Email of the user
+            hashed_password (str): Hashed password of the user
+        
+        Returns:
+            User: The newly created user object
+        """
+        # Create a new User object
+        new_user = User(email=email, hashed_password=hashed_password)
+        
+        # Add the new user to the session
+        self._session.add(new_user)
+        
+        # Commit the session to save the changes to the database
+        self._session.commit()
+        
+        return new_user

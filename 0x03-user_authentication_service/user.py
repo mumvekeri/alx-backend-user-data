02036c5037cc0058bf5b4466abc6121f6592dad4
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-06-11 20:56:56 +0200

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
-""" user.py answer """
-from sqlalchemy import Column, Integer, String, create_engine
+"""
+user.py module
+"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+)
 
 Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'users'
+    """
+    Definition of class User
+    """
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-# For demonstration purposes, here's how you might set up an in-memory SQLite database
-if __name__ == "__main__":
-    engine = create_engine('sqlite:///:memory:')
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    print(User.__tablename__)
-    for column in User.__table__.columns:
-        print("{}: {}".format(column, column.type))
